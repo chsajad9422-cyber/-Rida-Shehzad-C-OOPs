d2e3ef16c7456cd7348a1e09944f0ac8e3b1adaf
Rida Shehzad <chsajad9422@gmail.com>
2025-11-24 19:58:17 +0500

    Add files via upload

---
+++ dimond.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4; 
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        // Print stars
+        for (int j = 1; j <= 2*i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Bottom half of the pattern
+    for (int i = n-1; i >= 1; i--) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        // Print stars
+        for (int j = 1; j <= 2*i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
