cee965aa2590788f2a89b8af53c8d7f653e5fcd5
Rida Shehzad <chsajad9422@gmail.com>
2026-01-03 19:16:07 +0500

    Add files via upload

---
+++ Box.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Box{
+public:
+double lenght;
+double breadth;
+double height;
+};
+int main()
+{
+	Box Box1;
+	Box1.lenght=3;
+	Box1.breadth=5;
+	Box1.height=4;
+	cout<<"Lenght:"<<Box1.lenght<<endl;
+	cout<<"Breadth:"<<Box1.breadth<<endl;
+	cout<<"Height:"<<Box1.height<<endl;
+	cout<<"Rida Shehzad"<<endl;
+	cout<<"Roll No: 030"<<endl;
+	cout<<"semester:3rd"<<endl;
+	cout<<"Program: BSCE"<<endl;
+}
